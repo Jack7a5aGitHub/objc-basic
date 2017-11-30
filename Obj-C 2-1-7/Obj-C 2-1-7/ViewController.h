4f78a3bcd7afa3a653c9c3a2a50938f3aafe6842
Jack Wong <jack7a5a1993@icloud.com>
2017-11-30 15:21:19 +0900

    2-1-7完了しました。

---
+++ Obj-C 2-1-7/Obj-C 2-1-7/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Obj-C 2-1-7
+//
+//  Created by Jack Wong on 2017/11/29.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate>{
+    IBOutlet UITextField *textField;
+}
+
+@end
+
---
+++ Obj-C 2-1-7/Obj-C 2-1-7/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  Obj-C 2-1-7
+//
+//  Created by Jack Wong on 2017/11/29.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   textField.delegate = self;
+    textField.userInteractionEnabled = YES;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+    if (range.length + range.location > textField.text.length){
+        return NO;
+    }
+    
+    if ([string isEqualToString:@""] || string.length == 0){
+        return YES;
+    }
+    
+    NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    return newLength <= 30;
+}
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+@end
