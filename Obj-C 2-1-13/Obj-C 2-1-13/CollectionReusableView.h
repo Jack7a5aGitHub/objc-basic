9fbe1b288d92f1e0125a638f16ad261ccb5480d0
Jack Wong <jack7a5a1993@icloud.com>
2017-12-13 15:24:49 +0900

    2-1-13完了しました。

---
+++ Obj-C 2-1-13/Obj-C 2-1-13/CollectionReusableView.h
@@ -0,0 +1,14 @@
+//
+//  CollectionReusableView.h
+//  Obj-C 2-1-13
+//
+//  Created by Jack Wong on 2017/12/11.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionReusableView : UICollectionReusableView
+@property (weak, nonatomic) IBOutlet UILabel *headerLabel;
+
+@end
