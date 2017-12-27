beec29a306bc14a7ec745cbd419d4a17cc7c4bf1
Jack Wong <jack7a5a1993@icloud.com>
2017-12-27 17:18:02 +0900

    2-4-4 完了しました。

---
+++ Obj-C 2-4-4/Obj-C 2-4-4/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  Obj-C 2-4-4
+//
+//  Created by Jack Wong on 2017/12/27.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
