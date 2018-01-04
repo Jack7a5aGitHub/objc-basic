c7d865aa986b1b0ad12412358610679bd18e9697
Jack Wong <jack7a5a1993@icloud.com>
2018-01-04 10:38:59 +0900

    2-4-8

--- Obj-C 2-4-8/Obj-C 2-4-8/ViewController.m
+++ Obj-C 2-4-8/Obj-C 2-4-8/ViewController.m
@@ -7,10 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "Photos/Photos.h"
 
-@interface ViewController ()<UIDocumentInteractionControllerDelegate>
+@interface ViewController ()<UIDocumentInteractionControllerDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
-@property UIDocumentInteractionController *docController;
+@property UIDocumentInteractionController *doc;
+
+@property UIImagePickerController *imagePicker;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property NSData *imgData;
+
+@property UIImage *getImage;
 
 @end
 
@@ -18,7 +27,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self instaPostImage];
+    self.getImage = [UIImage imageNamed:@"photo0"];
+    self.imgData = UIImageJPEGRepresentation(self.getImage, 0.75f);
+    NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/image.igo"];
+    [self.imgData writeToFile:filePath atomically:YES];
+    
 }
 
 
@@ -26,32 +39,79 @@
     [super didReceiveMemoryWarning];
 }
 
--(void)instaPostImage{
-    
-    NSURL *url = [NSURL URLWithString:@"http://weather.livedoor.com/img/icon/1.gif"];
-    NSData *imageData = [[NSData alloc] initWithContentsOfURL:url];
-    UIImage *imageShare = [[UIImage alloc] initWithData: imageData];
-    NSURL * instagramURL = [NSURL URLWithString:@"instagram://app"];
-    
-    if ([[UIApplication sharedApplication] canOpenURL:instagramURL])
-    {
-        UIImage *imageToUse = imageShare;
-        NSString *documentDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
-        NSString *saveImagePath = [documentDirectory stringByAppendingPathComponent:@"Image.png"];
-        NSData *imageData = UIImagePNGRepresentation(imageToUse);
-        [imageData writeToFile:saveImagePath atomically:YES];
-        NSURL *imageURL = [NSURL fileURLWithPath:saveImagePath];
-        self.docController = [[UIDocumentInteractionController alloc] init];
-        self.docController = [UIDocumentInteractionController interactionControllerWithURL:imageURL];
-        self.docController.delegate = self;
-        self.docController.annotation = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"Testing"],@"InstagramCaption", nil];
-        self.docController.UTI = @"com.instagram.exclusivegram";
-        UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
-        [self.docController presentOpenInMenuFromRect:CGRectMake(1, 1, 1, 1) inView:vc.view animated:YES];
-    } else {
-        NSLog(@"Failed");
-    }
-
+//-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+//    self.getImage = info[UIImagePickerControllerOriginalImage];
+//    self.imgData = UIImageJPEGRepresentation(self.getImage, 0.75f);
+//    NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/getImage.igo"];
+//    [self.imgData writeToFile:filePath atomically:YES];
+//    self.imageView.image = [UIImage imageWithData:self.imgData];
+//    [self.imagePicker dismissViewControllerAnimated:YES completion:nil];
+//}
+- (IBAction)getPhoto:(id)sender {
+    self.imagePicker = [[UIImagePickerController alloc]init];
+    self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    self.imagePicker.delegate = self;
+    [self presentViewController:self.imagePicker animated:YES completion:nil];
 }
 
+- (IBAction)shareToIG:(id)sender {
+    
+//    NSURL *instagramURL = [NSURL URLWithString:@"instagram://app"];
+//    if([[UIApplication sharedApplication] canOpenURL:instagramURL])
+//    {
+//        CGFloat cropVal = (self.imageView.image.size.height > self.imageView.image.size.width ? self.imageView.image.size.width : self.imageView.image.size.height);
+//
+//        cropVal *= [self.imageView.image scale];
+//
+//        CGRect cropRect = (CGRect){.size.height = cropVal, .size.width = cropVal};
+//        CGImageRef imageRef = CGImageCreateWithImageInRect([self.imageView.image CGImage], cropRect);
+//
+//        NSData *imageData = UIImageJPEGRepresentation([UIImage imageWithCGImage:imageRef], 1.0);
+//        CGImageRelease(imageRef);
+//
+//        NSString *writePath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"instagram.igo"];
+//        if (![imageData writeToFile:writePath atomically:YES]) {
+//            // failure
+//            NSLog(@"image save failed to path %@", writePath);
+//            return;
+//        } else {
+//            // success.
+//        }
+//
+//        // send it to instagram.
+//        NSURL *fileURL = [NSURL fileURLWithPath:writePath];
+//        self.doc = [UIDocumentInteractionController interactionControllerWithURL:fileURL];
+//        self.doc.delegate = self;
+//        [self.doc setUTI:@"com.instagram.exclusivegram"];
+//        [self.doc setAnnotation:@{@"InstagramCaption" : @"We are making fun"}];
+//        [self.doc presentOpenInMenuFromRect:CGRectMake(0, 0, 320, 480) inView:self.view animated:YES];
+//    }
+//    else
+//    {
+//        NSLog (@"Instagram not found");
+//
+//    }
+//
+    
+    
+    NSString *filePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/image.igo"];
+    NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+    
+    
+//    NSString *imagePath = [NSString stringWithFormat:@"%@/image.igo", [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)lastObject]];
+//    [[NSFileManager defaultManager]removeItemAtPath:imagePath error:nil ];
+//    [UIImagePNGRepresentation(self.imageView.image) writeToFile:imagePath atomically:YES];
+    self.doc = [UIDocumentInteractionController interactionControllerWithURL:fileURL];
+    self.doc.UTI = @"com.instagram.exclusivegram";
+     self.doc.delegate = self;
+  //  [self.doc presentOpenInMenuFromRect:self.view.frame inView:self.view animated:YES];
+    UIApplication *application = [UIApplication sharedApplication];
+    NSURL *URL = [NSURL URLWithString:@"instagram://app"];
+    [application openURL:URL options:@{} completionHandler:^(BOOL success) {
+        if (success) {
+            NSLog(@"Opened url");
+            [self.doc presentOpenInMenuFromRect:self.view.frame inView:self.view animated:YES];
+        }
+    }];
+}
 @end
