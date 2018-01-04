29972ab867be9168bce0dd5e34a92876eba711c8
Jack Wong <jack7a5a1993@icloud.com>
2018-01-04 09:42:23 +0900

    2-4-6完了しました。

---
+++ Obj-C 2-4-6/Obj-C 2-4-6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Obj-C 2-4-6
+//
+//  Created by Jack Wong on 2017/12/29.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Obj-C 2-4-6/Obj-C 2-4-6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj-C 2-4-6
+//
+//  Created by Jack Wong on 2017/12/29.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
