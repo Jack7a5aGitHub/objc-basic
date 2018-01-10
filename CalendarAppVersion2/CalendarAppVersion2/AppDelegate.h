810bedac292a66ef84ec6f421bc0c519f1185542
Jack Wong <jack7a5a1993@icloud.com>
2018-01-10 11:58:26 +0900

    カレンダーVC作った

---
+++ CalendarAppVersion2/CalendarAppVersion2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalendarAppVersion2
+//
+//  Created by Jack Wong on 2018/01/10.
+//  Copyright © 2018 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalendarAppVersion2/CalendarAppVersion2/CalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarViewController.h
+//  CalendarAppVersion2
+//
+//  Created by Jack Wong on 2018/01/10.
+//  Copyright © 2018 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UIViewController
+
+@end
